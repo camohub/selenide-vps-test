10b379de896bffcd845a86a41ca8293f78d8528d
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-02-28 20:51:08 +0100

    TestWatcher implementation - fail() for screenshot.

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 pom.xml
 ==================
-- V súbore sú definované profily <profiles></profiles>, ktoré sú prepojené 
+- Profily <profiles></profiles>, ktoré sú prepojené 
     na anotácie @Category (fastTestCategory, fullTestcategory) 
     K tomu je nutné definovať premennú <parameters><category /><parameters/>
   
+
+TestWatcher
+=================
+- Trieda TestWatcher - je rozhranie, ktoré ponúka JUnit pre zachytávanie 
+  eventov ako je napr. fail. Tu sa používa na vytvorenie screenshotu 
+  keď test padne.
+
+
+Driver
+=================
+- Driver sa musí zatvoriť v triede TestWatcher::finish(), pretože
+  @After metoda sa zavolá pred TestWatcher::finish() 
+  a zavrie driver pred vytvorením screenshotu.  
+
--- src/test/java/tests/baseTest.java
+++ src/test/java/tests/baseTest.java
@@ -1,20 +1,14 @@
 package tests;
 
-import org.apache.commons.io.FileUtils;
+
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.junit.Rule;
 import org.openqa.selenium.WebDriver;
+import rules.TestWatcher;
 import services.WebDriverService;
-
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.util.Date;
-import java.util.Random;
 
 
 public class baseTest
@@ -24,44 +18,28 @@ public class baseTest
 
     protected String baseUrl = "http://localhost/seleniumweb";
 
-    protected Boolean testFail = false;  // If true make a screenshot
+
+    /**
+     * This is the JUnit way how to catch fail event.
+     */
+    @Rule
+    public TestWatcher failWatcher = new TestWatcher();
 
 
     @Before
     public void baseSetUp() throws MalformedURLException
     {
         driver = (new WebDriverService()).getDriver();
-        //driver = new ChromeDriver();
+
+        failWatcher.setDriver(driver);
     }
 
 
     @After
     public void baseTearDown() throws IOException
     {
-        makeScreenshot();  // Make screenshot
-
-        driver.close();
-        driver.quit();
-    }
-
-
-    protected void makeScreenshot() throws IOException
-    {
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshot, new File(getScreenshotName()));
-    }
-
-
-    /**
-     * Get date string for screenshot file name
-     * @return
-     */
-    protected String getScreenshotName()
-    {
-        String date = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
-        Integer rand = new Random().nextInt(10000);
-        String fileName = "./target/screenshots/" + getClass().getSimpleName() + "_" + date + "_" + rand + "_screenshot.png";
-
-        return fileName;
+        // Is implemented in TestWatcher::finish()
+        //driver.close();
+        //driver.quit();
     }
 }
